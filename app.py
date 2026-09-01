01be00897a4836c31184acb28de7e6f4479e9a41
Travis CI <travis@travis-ci.org>
2026-09-01 12:26:53 -0500

    Исправил отступы в app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-   import os
+import os
 
-   st.title("Мой рисунок")
+st.title("Мой рисунок")
 
-   if os.path.exists("image.png"):
-       st.image("image.png", caption="Это изображение из проекта")
-   else:
-       st.error("Файл image.png не найден в корневой папке.")
+if os.path.exists("image.png"):
+    st.image("image.png", caption="Это изображение из проекта")
+else:
+    st.error("Файл image.png не найден в корневой папке.")
