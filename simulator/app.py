32db66a0dd9d4ccf317674e70b1773892c7ff9f9
Travis CI <travis@travis-ci.org>
2026-09-01 12:53:37 -0500

    Исправил отступы в симуляторе

--- simulator/app.py
+++ simulator/app.py
@@ -1,9 +1,1 @@
-import streamlit as st
-   import os
-
-   st.title("Мой рисунок")
-
-   if os.path.exists("image.png"):
-       st.image("image.png", caption="Это изображение из проекта")
-   else:
-       st.error("Файл image.png не найден в корневой папке.")
+import streamlit as st import os st.title("Мой рисунок") if os.path.exists("image.png"): st.image("image.png", caption="Это изображение из проекта") else: st.error("Файл image.png не найден в корневой папке.")
